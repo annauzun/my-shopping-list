77584d2f990f1a0db76bcaf30611f2a8b2f90ff2
annauzun <terziann@mail.ru>
2023-11-06 05:06:51 -0600

    footer color changed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = (props) => {
   const { count } = props;
 
   return (
-    <div className="bg-sky-400 text-2xl text-white px-10 py-6">
+    <div className="bg-sky-500 text-2xl text-white px-10 py-6">
       <p className="flex flex-wrap items-center justify-center">
         Итого:<span className="mx-2 text-3xl font-semibold">{count}</span>
         наименований
